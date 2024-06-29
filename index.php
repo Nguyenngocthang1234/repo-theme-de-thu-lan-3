37eb5d445095e30bc7f51eac81ccdc5088eb7151
Nguyenngocthang1234 <thangbek123@gmail.com>
2024-06-29 14:52:27 +0700

    commit file code thử

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 id="h1">xin chào đến vs bình nguyen vo tan</h1>
+    <button id="buton"></button>
+    <script>
+        let h1 = document.getElementById('h1');
+       
+        
+    </script>
+    
+</body>
+</html>
