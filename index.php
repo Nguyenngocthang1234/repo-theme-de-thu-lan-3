a1669961b6d67942733457cae02bf547e035fc2d
Nguyenngocthang1234 <thangbek123@gmail.com>
2024-06-29 15:02:49 +0700

    thu code

--- index.php
+++ index.php
@@ -7,9 +7,14 @@
 </head>
 <body>
     <h1 id="h1">xin chào đến vs bình nguyen vo tan</h1>
-    <button id="buton"></button>
+    <button id="button">nhấn</button>
     <script>
         let h1 = document.getElementById('h1');
+        let button = document.getElementById('button');
+        button.onclick = function(){
+            h1.style.color ="red";
+        }
+
        
         
     </script>
